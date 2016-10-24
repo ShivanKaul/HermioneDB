c851c881060b19b04fd20f78962a486972fb0bdb
sangmoonhwang <ghkdtkdans@gmail.com>
2016-10-24 17:23:48 -0400

    Fix scripts. Now ctrl_c command while runServer.sh is running kills rmiregistry process

--- runServer.sh
+++ runServer.sh
@@ -1,5 +1,14 @@
-rmiregistry&;
-echo -e "Process running rmi registry: "
-ps | grep rmiregistry
+#!/bin/bash
 
-java -classpath je-3.3.75.jar:. -Djava.rmi.server.codebase=file:. org.mcgill.ecse420.f2016.MasterImpl $1
+# trap ctrl-c and call ctrl_c()
+trap ctrl_c INT
+
+ctrl_c() {
+        echo "** Trapped CTRL-C.. Killing rmiregistry process..."
+        kill `ps -a | grep rmi | cut -d ' ' -f 1`
+    }
+rmiregistry &
+echo -e "Process running rmi registry: ";
+ps | grep rmiregistry;
+
+java -classpath je-3.3.75.jar:. -Djava.rmi.server.codebase=file:. org.mcgill.ecse420.f2016.MasterImpl $1;
