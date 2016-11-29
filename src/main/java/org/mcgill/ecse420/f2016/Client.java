404b270ce39cedccfe277941761f9c151d085bdc
ShivanKaul <shivankaul.1993@gmail.com>
2016-11-29 17:23:33 -0500

    Client side caching wip

--- src/main/java/org/mcgill/ecse420/f2016/Client.java
+++ src/main/java/org/mcgill/ecse420/f2016/Client.java
@@ -3,8 +3,12 @@ package org.mcgill.ecse420.f2016;
 import com.sleepycat.je.DatabaseEntry;
 
 import java.io.UnsupportedEncodingException;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 
@@ -14,6 +18,9 @@ import java.util.Scanner;
 public class Client {
 
   static Registry registry;
+  // We eventually need to cache the hash object that gives us the worker
+  // For now we just have simple map from key (dbname + key) to worker IP
+  static Map<String, String> cache = new HashMap<>();
 
   private Client() {
   }
@@ -47,9 +54,17 @@ public class Client {
           loop = false;
           continue;
         }
+
         Result response = promptResult.result();
+        // If GET and server says doesn't exist
+        if (promptResult.method().equals(PromptResult.Method.GET) && !response.workerPoolStatus && !response.dbStatus) {
+          System.out.println("Server responded with null! Are you sure the key you're " +
+                  "looking for exists?");
+          continue;
+        }
         if (response.noErrors()) {
-          if (response.returnedValue == null) {
+          // If set was the method and there's no errors but the no returned value
+          if (promptResult.method() == PromptResult.Method.SET && response.returnedValue == null) {
             System.out.println("Successfully set!");
             continue;
           }
@@ -77,30 +92,56 @@ public class Client {
       if (tokens.length != 3) {
         throw new IllegalArgumentException("get takes in only 2 arguments - table name and key");
       }
-      promptResult = new PromptResult((handleGet(tokens[1], tokens[2], stub)));
+      promptResult = new PromptResult((handleGet(tokens[1], tokens[2], stub)), PromptResult.Method.GET);
     } else if (tokens[0].equals("set")) {
       if (tokens.length != 4) {
         throw new IllegalArgumentException("set takes in only 3 arguments - table name, key and value");
       }
-      promptResult = new PromptResult((handleSet(tokens[1], tokens[2], tokens[3], stub)));
+      promptResult = new PromptResult((handleSet(tokens[1], tokens[2], tokens[3], stub)), PromptResult.Method.SET);
     } else if (tokens[0].equals("exit")) {
       promptResult = new PromptResult(false);
     }
     return promptResult;
   }
 
-  private static Result handleGet(String table, String key, Master stub) throws Exception {
+  private static Result handleGet(String table, String key, Master master_stub) throws Exception {
     // Get a handle to the worker from the master
     // Talk to the worker directly
-    Result response = stub.getWorkerHost(table + "_" + key);
-    if (response.noErrors()) {
-      String workerAddress = response.returnedValue;
-      // Connect to worker
-      registry = LocateRegistry.getRegistry(workerAddress);
-      Worker worker = (Worker) registry.lookup("Worker");
+    String compositeKey = table + "_" + key;
+    if (!cache.containsKey(compositeKey)) {
+      Result response = master_stub.getWorkerHost(compositeKey);
+      if (response.noErrors()) {
+        String workerAddress = response.returnedValue;
+        Worker worker = getWorkerFromAddress(workerAddress);
+        // Put in cache
+        cache.put(compositeKey, workerAddress);
+        return worker.get(key);
+      } else return response;
+    } else { // cache does contain key
+      String workerAddress = cache.get(compositeKey);
+      Worker worker;
+      try {
+        worker = getWorkerFromAddress(workerAddress);
+      } catch (Exception e) {
+        // failed, just get key from master
+        Result response = master_stub.getWorkerHost(compositeKey);
+        if (response.noErrors()) {
+          workerAddress = response.returnedValue;
+          worker = getWorkerFromAddress(workerAddress);
+          // Put in cache
+          cache.put(compositeKey, workerAddress);
+          return worker.get(key);
+        } else return response;
+      }
       return worker.get(key);
-    } else return response;
+    }
   }
+
+  private static Worker getWorkerFromAddress(String workerAddress) throws RemoteException, NotBoundException {
+    registry = LocateRegistry.getRegistry(workerAddress);
+    return (Worker) registry.lookup("Worker");
+  }
+
   private static Result handleSet(String table, String key, String value, Master stub) throws Exception {
     // Get a handle to the worker from the master
     // Talk to the worker directly
@@ -119,10 +160,16 @@ public class Client {
 
 class PromptResult {
   private Result result = null;
+  private Method method;
   private boolean continueLoop = true;
 
-  public PromptResult(Result result) {
+  public enum Method {
+    GET, SET
+  }
+
+  public PromptResult(Result result, Method method) {
     this.result = result;
+    this.method = method;
   }
 
   public PromptResult(boolean continueLoop) {
@@ -135,6 +182,10 @@ class PromptResult {
 
   public Result result() {
     return this.result;
+  }
+
+  public Method method() {
+    return this.method;
   }
 
 }
