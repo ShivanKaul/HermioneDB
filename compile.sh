80ae97f288c2a727230c2b31c80cf326dd0fd043
sangmoonhwang <ghkdtkdans@gmail.com>
2016-11-28 23:55:37 -0500

    fix runServer.sh

--- compile.sh
+++ compile.sh
@@ -2,4 +2,5 @@ destDir=/tmp/berkeleydb/
 mkdir -p $destDir
 javac -d $destDir -cp ~/.m2/repository/com/sleepycat/je/3.3.75/je-3.3.75.jar src/main/java/org/mcgill/ecse420/f2016/Configs/*.java src/main/java/org/mcgill/ecse420/f2016/*.java
 cp *sh $destDir
+cp *jar $destDir
 echo "Please go to $destDir and run the software using runServer.sh and runClient.sh"
